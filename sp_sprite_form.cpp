5c51e2741fbea7e85251dd231921ba8ea9b0cb8a
Mike Lindner <mlindner9@yahoo.com>
2021-02-17 19:40:27 -0500

    Change namespace to sp.

---
+++ sp_sprite_form.cpp
@@ -0,0 +1,21 @@
+//
+// Dec-2020, Michael Lindner
+// MIT license
+//
+#include "sp_sprite_form.h"
+
+
+namespace sp
+{
+///////////////////
+
+void SpriteForm::setSize(PixDim size)
+{
+   const bool isDegenerate = m_size.x == 0.f || m_size.y == 0.f;
+   const glm::vec2 rotCenterRatio =
+      isDegenerate ? glm::vec2(.5f, .5f) : (m_rotCenter / m_size);
+   m_size = size;
+   m_rotCenter = m_size * rotCenterRatio;
+}
+
+} // namespace sp
